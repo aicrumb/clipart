cc9abaee0add2fadbb1f5baf21803498f4a86c39
bunnybelle84 <bunnybelle84@gmail.com>
2022-05-11 15:05:33 -0500

    param tweak & fix microwave

--- bake.py
+++ bake.py
@@ -12,8 +12,8 @@ wandb.init(project="guide-optimization")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 stages = 10
-resolution = (64,64)
-steps = int(250/stages)
+resolution = (128,128)
+steps = int(200/stages)
 clip_model = clip.load("ViT-B/32",jit=False)[0].to(device)
 
 timeout = 0.015  # sleep a little, dont run everything so fast, my computer will overheat doing this for extended time
@@ -26,12 +26,11 @@ z = torch.randn((1,3,resolution[0],resolution[1]), requires_grad=True, device=de
 _, norm = render_norm(prompt, z)
 del render_norm
 
-
 fix = transforms.Resize(224)
 guides = [torch.randn((512), device=device) for i in range(stages)]
 for i in range(len(guides)):
     guides[i].requires_grad = True
-guide_opt = optim.Adam(guides, 0.1)
+guide_opt = optim.AdamW(guides, 0.12)
 
 steps = 100
 for step in trange(steps):
@@ -39,6 +38,11 @@ for step in trange(steps):
     loss = 0
     for i in range(len(guides)):
         z, pz = render(guides[i], z)
+
+        # a loss to push it in the right semantic direction, but it's magnitude decays over time so
+        # that in the early bit it doesnt have to focus on pushin it in the right direction
+        # on it's own, but in the late bit it can focus on the things that are more important than
+        # being globally semantically close
         loss+=(sph_dist(guides[i], prompt)/(len(guides)))*(1-step/steps)*(1-i/len(guides))
     z_emb = clip_model.encode_image(fix(z))
     loss += sph_dist(z_emb, prompt)
@@ -47,10 +51,16 @@ for step in trange(steps):
     loss.backward(retain_graph=True) 
     guide_opt.step()
 
-    # idk why the image isnt updating
+    # idk why the image isnt updating so lets save and reopen it ??? that's gotta be a big time waster
+    # but honestly it's the one thing that's fixed it so far
+
     pz.save("output.png")
     pz = Image.open("output.png")
-    wandb.log({"loss": loss.item(), "output": [wandb.Image(pz, caption=prompt_o+" (ex)"), wandb.Image(norm, caption=prompt_o+" (og)")]})
+    wandb.log({"loss": loss.item(), 
+               "output": [
+                            wandb.Image(pz, caption=prompt_o+" (ex)"), 
+                            wandb.Image(norm, caption=prompt_o+" (og)")
+                          ]})
 
 name=prompt_o.replace(" ", "_")
 torch.save(torch.stack(guides), f"baked/{name}.pt")
--- microwave.py
+++ microwave.py
@@ -3,16 +3,20 @@ import clip
 from clipart import CLIPart
 import torch 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-prompt = sys.argv[1] if ".pth" not in sys.argv[1] else None
-path = sys.argv[2] if prompt else sys.argv[1]
-path = torch.open(path).to(device)
+prompt = sys.argv[1] if ".pt" not in sys.argv[1] else None
+# path = sys.argv[2] if prompt else sys.argv[1]
+if prompt:
+	path = sys.argv[2]
+else:
+	path = sys.argv[1]
+path = torch.load(path).to(device)
 steps = 25
 stages = [i for i in path]
 clip_model = clip.load("ViT-B/32", jit=False)[0].to(device)
 render = CLIPart(steps=steps, clip_model=clip_model, device=device, lr=0.12)
 def lerp(x,y,t):
     return (1 - t) * x + t * y
-z = torch.randn((512), device=device, requires_grad=True)
+z = torch.randn((1,3,224,224), device=device, requires_grad=True)
 
 if prompt:
 	embedding = clip_model.encode_text(clip.tokenize(prompt).to(device))
@@ -20,7 +24,7 @@ if prompt:
 for stage in stages:
 	if prompt:
 		z, pz = render(lerp(stage, embedding, .5), z)
-	else
+	else:
 		z, pz = render(stage, z)
-		
+
 pz.save("output.png")
