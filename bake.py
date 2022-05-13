ced11a1de2297eb047d0db57d6c4f81d726fee40
Chloe <spronkoid@gmail.com>
2022-05-13 01:17:00 -0500

    ability to learn information that's not in CLIP
    instead of training just a word with bake, rename an image (.jpg or .png) to the caption, with underscores instead of spaces (keep the extension), and just feed it the filename.
    
    I'm only doing this so that I can generate among us characters

--- bake.py
+++ bake.py
@@ -8,7 +8,7 @@ from torchvision import transforms
 import wandb
 import sys
 from PIL import Image
-wandb.init(project="guide-optimization")
+wandb.init(project="10STAGE-metaclip")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 stages = 10
@@ -16,17 +16,23 @@ resolution = (128,128)
 steps = int(200/stages)
 clip_model = clip.load("ViT-B/32",jit=False)[0].to(device)
 
-timeout = 0.015  # sleep a little, dont run everything so fast, my computer will overheat doing this for extended time
-render = CLIPart(clip_model=clip_model, device=device, steps=steps, cutn=2, verbose=False, timeout=timeout, lr=0.15)
-render_norm = CLIPart(clip_model=clip_model, device=device, steps=steps*stages, cutn=2, verbose=False, timeout=timeout)
+timeout = 0.01  # sleep a little, dont run everything so fast, my computer will overheat doing this for extended time
+render = CLIPart(clip_model=clip_model, device=device, steps=steps, cutn=1, verbose=False, timeout=timeout, lr=0.12)
+render_norm = CLIPart(clip_model=clip_model, device=device, steps=steps*stages, cutn=1, verbose=False, timeout=timeout)
 prompt_o = sys.argv[1]
 print(f"Baking {prompt_o}")
-prompt = clip_model.encode_text(clip.tokenize(prompt_o).to(device))
+fix = transforms.Resize(224)
+if ".jpg" not in prompt_o and ".png" not in prompt_o:
+    prompt = clip_model.encode_text(clip.tokenize(prompt_o).to(device))
+else:
+    prompt = clip_model.encode_image(fix(transforms.ToTensor()(Image.open(prompt_o).convert("RGB")).unsqueeze(0)).to(device))
+    prompt_o = prompt_o.split("/")[-1].replace("_", " ").replace(".jpg", "").replace(".png", "")
+    
+
 z = torch.randn((1,3,resolution[0],resolution[1]), requires_grad=True, device=device)
 _, norm = render_norm(prompt, z)
 del render_norm
 
-fix = transforms.Resize(224)
 guides = [torch.randn((512), device=device) for i in range(stages)]
 for i in range(len(guides)):
     guides[i].requires_grad = True
